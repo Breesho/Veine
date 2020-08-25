f8ee0f7eb3ac717d93842637cce056b7939f9873
Breesho <felix.noel3@orange.fr>
2020-08-25 16:52:52 +0200

    maj

--- App-Veine/model/model_contact.php
+++ App-Veine/model/model_contact.php
@@ -29,7 +29,7 @@ class Contacts
         ON lhp4_contacts.contacts_id = have_contacts.contacts_id
         INNER JOIN lhp4_users
         ON lhp4_users.users_id = lhp4_contacts.users_id
-        WHERE have_contacts.users_id = :users_id';
+        WHERE have_contacts.users_id = :users_id AND contacts_authorized = 1';
 
         try {
 
@@ -50,8 +50,37 @@ class Contacts
             die('Erreur : ' . $e->getMessage());
         }
     }
-    
-     /**
+    public function getWaitingContacts($userId)
+    {
+        $query = 'SELECT have_contacts.users_id, users_pseudo as contact_pseudo, lhp4_contacts.users_id as contactPseudo_id, lhp4_contacts.contacts_id as contact_id, contacts_authorized, contacts_bookmark
+        FROM veine.have_contacts
+        INNER JOIN lhp4_contacts
+        ON lhp4_contacts.contacts_id = have_contacts.contacts_id
+        INNER JOIN lhp4_users
+        ON lhp4_users.users_id = lhp4_contacts.users_id
+        WHERE have_contacts.users_id = :users_id AND contacts_authorized = 0';
+
+        try {
+
+            $resultQuery = $this->bdd->prepare($query);
+            $resultQuery->bindValue(':users_id', $userId);
+            $resultQuery->execute();
+
+            $resultContacts = $resultQuery->fetchAll();
+
+            if ($resultContacts) {
+
+                return $resultContacts;
+            } else {
+
+                return false;
+            }
+        } catch (Exception $e) {
+            die('Erreur : ' . $e->getMessage());
+        }
+    }
+
+    /**
      * Méthode pour valider un contact de la liste d'attente
      * @param type integer
      * 
@@ -77,7 +106,7 @@ class Contacts
         }
     }
 
-     /**
+    /**
      * Méthode pour Ajouter un contact au favoris
      * @param type integer
      * 
@@ -123,7 +152,7 @@ class Contacts
             die('Erreur : ' . $e->getMessage());
         }
     }
-       /**
+    /**
      * Méthode pour supprimer un contact des contacts
      * @param type integer
      * 
@@ -146,7 +175,7 @@ class Contacts
             die('Erreur : ' . $e->getMessage());
         }
     }
-       /**
+    /**
      * Méthode pour supprimer une demande de contact en attentes
      * @param type integer
      * 
@@ -162,6 +191,41 @@ class Contacts
 
             if ($resultQuery->execute()) {
                 return true;
+            } else {
+                return false;
+            }
+        } catch (Exception $e) {
+            die('Erreur : ' . $e->getMessage());
+        }
+    }
+
+    public function SearchContact($contactId)
+    {
+        $query = 'SELECT lhp4_users.users_id AS contact_id, have_contacts.users_id, users_pseudo as contact_pseudo, lhp4_contacts.contacts_id AS table_contact_id,
+        CASE 
+            WHEN `lhp4_contacts`.`contacts_bookmark` = 1 AND have_contacts.users_id = :contact_id THEN 1
+            ELSE 0
+        END AS `bookmarked`,
+        CASE 
+            WHEN `lhp4_contacts`.`contacts_authorized` = 1 AND have_contacts.users_id = :contact_id THEN 1
+            WHEN `lhp4_contacts`.`contacts_authorized` IS NOT NULL AND have_contacts.users_id = :contact_id THEN 0
+        END AS `authorized`
+        FROM veine.lhp4_users
+        LEFT JOIN `lhp4_contacts`
+        ON `lhp4_contacts`.users_id = lhp4_users.users_id
+        LEFT JOIN have_contacts
+        ON lhp4_contacts.contacts_id = have_contacts.contacts_id
+        WHERE lhp4_users.users_id != :contact_id';
+
+        try {
+
+            $resultQuery = $this->bdd->prepare($query);
+            $resultQuery->bindValue(':contact_id', $contactId);
+            
+            
+            
+            if ($resultQuery->execute()) {
+                return $resultQuery->fetchAll();
             } else {
                 return false;
             }
--- App-Veine/view/contact.php
+++ App-Veine/view/contact.php
@@ -2,6 +2,7 @@
 
 require_once '../controller/ContactController.php';
 
+var_dump($_POST);
 
 ?>
 <!DOCTYPE html>
@@ -24,70 +25,122 @@ require_once '../controller/ContactController.php';
         <div class="logo">
             <figure></figure>
         </div>
-
+      
         <form action="" method="post">
+            
+            <div id="container-contactPending">
+              
+                <div class="container-input">
 
+                    <input type="text" id="searchInput" name="searchInput" value="" placeholder="Recherche Contact">
+                    <button type="submit" id="searchBtn" name="searchBtn" value=""><i class="fas fa-search"></i></button>
+
+                </div>
+            </div>
+            <?php if (!isset($_POST['searchBtn'])) { ?>
             <div id="container-contactPending">
                 <div class="title">
-                    <h3>Contact en attente</h3>
+                    <h3>Contact(s) en attente</h3>
                 </div>
                 <ul>
-                <?php if (!empty($contactsArray)) {
-                   foreach ($contactsArray as $contact) {
-                        if ($contact['contacts_authorized'] == 1) {
-                            continue;
-                        } ?>
+                    <?php if (!empty($waitingContactsArray)) {
+                        foreach ($waitingContactsArray as $contact) { ?>
+                           
+
+                            <li class="contact-center">
+                                <div class="pseudo-contact">
+                                    <h4><?= $contact['contact_pseudo'] ?> </h4>
+                                </div>
+                                <div class="container-btn">
+                                    <button type="submit" name="accepted" value="<?= $contact['contact_id'] ?>"><i class="fas fa-check"></i>
+                                    </button> <button type="submit" name="refused" value="<?= $contact['contact_id'] ?>"><i class="fas fa-times"></i></button>
+                                </div>
+                            </li>
+
+                      
+                   <?php }} else { ?>
 
                         <li class="contact-center">
-                            <div class="pseudo-contact">
-                                <h4><?= $contact['contact_pseudo'] ?> </h4>
-                            </div>
-                            <div class="container-btn">
-                                <button type="submit" name="accepted" value="<?= $contact['contact_id'] ?>"><i class="fas fa-check"></i>
-                                </button> <button type="submit" name="refused" value="<?= $contact['contact_id'] ?>"><i class="fas fa-times"></i></button>
-                            </div>
+                            <h4>Pas de nouvelle demande</h4>
                         </li>
 
-                    <?php }} else { ?>
-
-<p>caca</p>
                     <?php } ?>
                 </ul>
             </div>
 
             <div id="container-contacts">
                 <div class="title">
-                    <h3>Contact</h3>
+                    <h3>Contact(s)</h3>
                 </div>
                 <ul>
-                <?php if (!empty($contactsArray)) {
-                    foreach ($contactsArray as $contact) {
-                        if ($contact['contacts_authorized'] == 0) {
-                            continue;
-                        } ?>
+                    <?php if (!empty($contactsArray)) {
+                        foreach ($contactsArray as $contact) { ?>
+                           
 
-                        <li class="contact-center">
-                        <div class="pseudo-contact">
-                                <h4><?= $contact['contact_pseudo'] ?> </h4>
-                        </div>
-                        <div class="container-btn">
-                             <button type="submit" name="delete" value="<?= $contact['contact_id'] ?>"><i class="fas fa-trash-alt"></i></button>
-                            <?php if ($contact['contacts_bookmark'] == 0) { ?>
-                                <button type="submit" name="bookmarked" value="<?= $contact['contact_id'] ?>"><i class="far fa-star"></i></button>
-                            <?php   } else { ?>
-
-                                <button type="submit" name="unmarked" value="<?= $contact['contact_id'] ?>"><i class="fas fa-star"></i></button>
-     
+                            <li class="contact-center">
+                                <div class="pseudo-contact">
+                                    <h4><?= $contact['contact_pseudo'] ?> </h4>
                                 </div>
-                        </li>
-                            <?php }}} else { ?>
+                                <div class="container-btn">
+                                    <button type="submit" name="delete" value="<?= $contact['contact_id'] ?>"><i class="fas fa-trash-alt"></i></button>
+                                    <?php if ($contact['contacts_bookmark'] == 0) { ?>
+                                        <button type="submit" name="bookmarked" value="<?= $contact['contact_id'] ?>"><i class="far fa-star"></i></button>
+                                    <?php   } else { ?>
 
-                            <p>pipi</p>
-                            <?php } ?>
-                    
+                                        <button type="submit" name="unmarked" value="<?= $contact['contact_id'] ?>"><i class="fas fa-star"></i></button>
+                                <?php } ?>
+                                </div>
+                            </li>
+                  
+                               
+                             <?php }} else { ?>
+
+                    <li class="contact-center">
+                        <h4>Votre liste de contact est vide </h4>
+                    </li>
+
+
+                <?php } ?>
+
                 </ul>
             </div>
+            <?php } else { ?>
+                <div>
+                <div id="container-contacts">
+                    <ul>
+                    <?php if (!empty($UsersArray)) {
+                        foreach ($UsersArray as $Users) { ?>
+                           
+                            <li class="contact-center">
+                                <div class="pseudo-contact">
+                                    <h4><?= $Users['contact_pseudo'] ?> </h4>
+                                </div>
+                                <div class="container-btn">
+                                    
+                                    <?php if ($Users['authorized'] == null) { ?>
+                                        <button type="submit" name="add" value="<?= $Users['table_contact_id'] ?>"><i class="fas fa-plus"></i></button>
+                                    <?php   } elseif ($Users['authorized'] == 1) { ?>
+                                        <button type="submit" name="delete" value="<?= $Users['table_contact_id'] ?>"><i class="fas fa-trash-alt"></i></button>
+                                
+                                        <?php } elseif ($Users['authorized'] == 0) { ?>
+                                            <button type="submit" name="accepted" value="<?= $Users['table_contact_id'] ?>"><i class="fas fa-check"></i>
+                                            </button> <button type="submit" name="refused" value="<?= $Users['table_contact_id'] ?>"><i class="fas fa-times" id="testtesmort"></i></button>
+                                       <?php } ?>
+                                </div>
+                            </li>
+                  
+                               
+                             <?php }} else { ?>
 
+                    <li class="contact-center">
+                        <h4>Votre liste de contact est vide </h4>
+                    </li>
+
+
+                <?php } ?>
+                    </ul>
+                </div>
+            <?php } ?>
         </form>
 
     </div>
